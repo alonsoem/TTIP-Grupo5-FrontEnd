e04c6b21231995607f5c5ce007af0afad87448fa
Asta86 <psljp@protonmail.com>
2021-09-18 11:03:51 -0300

    Update styles, remove unused imports

--- src/components/NavBarPage.jsx
+++ src/components/NavBarPage.jsx
@@ -1,101 +1,53 @@
-import React, { Component} from "react";
-import { NavLink} from 'react-router-dom';
-import {Navbar, Nav,Form,  Button,NavItem} from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
-
-import './NavBar.css';
-import { Modal } from "react-bootstrap";
-import { withTranslation } from 'react-i18next';
-import i18n from "../i18n";
-
-
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { withTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 import App18 from "../App18.jsx";
+import "./NavBar.css";
 
 class NavBarPage extends Component {
-  state = {
-    isOpen: false,
-    notification_qty:0,
-  };
+  render() {
+    const { t } = this.props;
 
-  openModal = () => this.setState({ isOpen: true });
-  closeModal = () => this.setState({ isOpen: false });
-
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-
-  readNotifications(){
-
-    this.notification();
-  }
-
-  notification(){
-    if (this.state.notification_qty>0) {
-      toast("Hay " + this.state.notification_qty + " nuevas notificaciones!!");
-      this.setState({notification_qty:0});
-    }
-  }
-
-render() {
-  const { t } = this.props;
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
-
-  return (
-
+    return (
       <div>
-  <Navbar  collapseOnSelect expand="md" bg="light" variant="light" className="navbar-light heavy-rain-gradient darken-3 mb-4">
-      <Navbar.Brand href="/"><img src={process.env.PUBLIC_URL + '/alec-icon.png'} alt="Logo" id="logo" /></Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav " />
+        <Navbar
+          collapseOnSelect
+          expand="md"
+          className="navbar-light heavy-rain-gradient darken-3 mb-4"
+        >
+          <Navbar.Brand>
+            <img
+              className="img-thumbnail"
+              src={process.env.PUBLIC_URL + "/alec-logo.png"}
+              alt="Logo"
+              id="logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-      <Navbar.Collapse id="responsive-navbar-nav ">
-        <Nav className="mr-auto justify-content-center ">
-          <NavItem ><NavLink to="/login" > {t("navLogin")} </NavLink></NavItem>
-          <NavItem><NavLink to="/taxes"> {t("navTaxes")}</NavLink></NavItem>
-          <NavItem><App18/></NavItem>
-        </Nav>
-
-      </Navbar.Collapse>
-  </Navbar>
-
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable={false}
-            pauseOnHover
-        />
-
-
-
-        <Modal size="sm" show={this.state.isOpen} onHide={this.closeModal} >
-          <Modal.Header closeButton>
-            <Modal.Title>Su Token</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Recuerde que precederlo por Bearer:</Form.Label>
-              <Form.Control as="textarea" value={localStorage.token} rows={8} />
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={this.closeModal}>
-              Close
-            </Button>
-
-          </Modal.Footer>
-        </Modal>
-  </div>
+          <Navbar.Collapse id="responsive-navbar-nav ">
+            <Nav className="mr-auto justify-content-center ">
+              <NavItem>
+                <NavLink className="btn btn-primary" to="/login">
+                  {t("navLogin")}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="btn btn-primary" to="/taxes">
+                  {t("navTaxes")}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <App18 />
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
 
-export default withTranslation() (NavBarPage);
+export default withTranslation()(NavBarPage);
