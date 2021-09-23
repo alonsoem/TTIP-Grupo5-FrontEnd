ff8077b149e5a534f30d3d4bf44b447982c73db1
Asta86 <psljp@protonmail.com>
2021-09-23 09:59:42 -0300

    Restore calculator background

--- src/components/MainCalc.jsx
+++ src/components/MainCalc.jsx
@@ -32,8 +32,8 @@ class MainCalc extends Component {
   };
 
   componentDidMount() {
-    if (!localStorage.getItem('token')) {
-      this.props.history.push('/login');
+    if (!localStorage.getItem("token")) {
+      this.props.history.push("/login");
     }
   }
 
@@ -58,120 +58,115 @@ class MainCalc extends Component {
   handleAPIError(responseError) {
     let errorToDisplay = this.props.t("genericError");
 
-    if (responseError.request && responseError.request.status===0){
+    if (responseError.request && responseError.request.status === 0) {
       errorToDisplay = this.props.t("comError");
     }
     this.setState({ error: errorToDisplay });
     this.showAlert();
-
   }
 
   render() {
     const { t } = this.props;
 
     return (
-        <div>
-          <NavBarPage />
-          <div className="container-fluid">
-
-        <div className="container">
-
-          <Row className="padding-5 justify-content-center">
-            <Col className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
-              <MDBCard>
-                <MDBCardBody>
-                  <Form onSubmit={this.handleSubmit}>
-                    <div className="card-header">
-                      <h4 className="text-center text-monospace text-black">
-                        {t("maincalcTitle")}
-                      </h4>
-                    </div>
-                    <br />
-                    <Alert
-                      className="alert alert-dismissible"
-                      variant="danger"
-                      show={this.state.errorVisible}
-                    >
-                      {this.state.error}
-                    </Alert>
-                    <FormGroup controlId="formBasicAmount">
-                      <MDBInput
-                        type="text"
-                        label={t("amount")}
-                        onChange={this.handleChangeAmount}
-                      />
-                    </FormGroup>
-
-                    <FormGroup controlId="formBasicPurchaseType">
-                      <label htmlFor="purchaseType" className="form-label">
-                        {t("purchaseType")}
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          href="http://biblioteca.afip.gob.ar/pdfp/RG_4240_AFIP_A2.pdf"
-                        >
-                          {t("typeReference")}
-                        </a>
-                      </label>
-                      <select
-                        name="purchaseType"
-                        className="form-control"
-                        id="purchaseType"
-                        onChange={this.handleChangePurchaseType}
+      <div id="maincalcdiv" className="container-fluid bg">
+        <NavBarPage />
+        <div className="container-fluid">
+          <div className="container">
+            <Row className="padding-5 justify-content-center">
+              <Col className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                <MDBCard>
+                  <MDBCardBody>
+                    <Form onSubmit={this.handleSubmit}>
+                      <div className="card-header">
+                        <h4 className="text-center text-monospace text-black">
+                          {t("maincalcTitle")}
+                        </h4>
+                      </div>
+                      <br />
+                      <Alert
+                        className="alert alert-dismissible"
+                        variant="danger"
+                        show={this.state.errorVisible}
                       >
-                        <option value="NOAPARTADO">{t("none")}</option>
-                        <option value="APARTADOA">A</option>
-                        <option value="APARTADOB">B</option>
-                      </select>
-                    </FormGroup>
-
-                    <Row className="justify-content-center">
-                      <Col className="justify-content-middle text-center">
-                        <Button
-                          variant="primary"
-                          type="submit"
-                          className="align-content-center"
-                        >
-                          {t("send")}
-                        </Button>
-                      </Col>
-                    </Row>
-                    <br />
-                    <Row className="justify-content-left">
-                      <Col className="text-center">
+                        {this.state.error}
+                      </Alert>
+                      <FormGroup controlId="formBasicAmount">
                         <MDBInput
                           type="text"
-                          label={t("result")}
-                          readOnly
-                          value={this.state.result}
+                          label={t("amount")}
+                          onChange={this.handleChangeAmount}
                         />
-                      </Col>
-                    </Row>
-                    <br />
+                      </FormGroup>
 
-                  </Form>
-                </MDBCardBody>
-              </MDBCard>
-            </Col>
-            <Col
-              id="appliedTaxesDiv"
-              className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4"
-            >
-              <div className="card text-white bg-info">
-                <div className="card-header">
-                  <h4>{t("appliedTaxes")}</h4>
+                      <FormGroup controlId="formBasicPurchaseType">
+                        <label htmlFor="purchaseType" className="form-label">
+                          {t("purchaseType")}
+                          <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="http://biblioteca.afip.gob.ar/pdfp/RG_4240_AFIP_A2.pdf"
+                          >
+                            {t("typeReference")}
+                          </a>
+                        </label>
+                        <select
+                          name="purchaseType"
+                          className="form-control"
+                          id="purchaseType"
+                          onChange={this.handleChangePurchaseType}
+                        >
+                          <option value="NOAPARTADO">{t("none")}</option>
+                          <option value="APARTADOA">A</option>
+                          <option value="APARTADOB">B</option>
+                        </select>
+                      </FormGroup>
+
+                      <Row className="justify-content-center">
+                        <Col className="justify-content-middle text-center">
+                          <Button
+                            variant="primary"
+                            type="submit"
+                            className="align-content-center"
+                          >
+                            {t("send")}
+                          </Button>
+                        </Col>
+                      </Row>
+                      <br />
+                      <Row className="justify-content-left">
+                        <Col className="text-center">
+                          <MDBInput
+                            type="text"
+                            label={t("result")}
+                            readOnly
+                            value={this.state.result}
+                          />
+                        </Col>
+                      </Row>
+                      <br />
+                    </Form>
+                  </MDBCardBody>
+                </MDBCard>
+              </Col>
+              <Col
+                id="appliedTaxesDiv"
+                className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4"
+              >
+                <div className="card text-white bg-info">
+                  <div className="card-header">
+                    <h4>{t("appliedTaxes")}</h4>
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">Impuesto 1</h5>
+                    <p className="card-text text-white">Resumen</p>
+                  </div>
                 </div>
-                <div className="card-body">
-                  <h5 className="card-title">Impuesto 1</h5>
-                  <p className="card-text text-white">Resumen</p>
-                </div>
-              </div>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
-        </div>
-
     );
   }
 }
