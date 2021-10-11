0bb0bf2e578dfdd2a87e73248644761f695d36c6
Asta86 <psljp@protonmail.com>
2021-10-11 12:56:34 -0300

    Fix eslint errors

--- src/components/brokerEdit.jsx
+++ src/components/brokerEdit.jsx
@@ -1,44 +1,38 @@
 import React from "react";
-import { withTranslation } from "react-i18next";
-import {getBroker, postTaxCreate} from "../api/api";
-import NavBarPage from "./NavBarPage";
-import {Card, Row, Form, Button, Image,Col} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
-
+import { withTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import { getBroker, postTaxCreate } from "../api/api";
+import NavBarPage from "./NavBarPage";
 
 class Taxes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id:0,
+      id: 0,
       name: "",
-      taxes:[],
-      broker:null,
+      taxes: [],
+      broker: null,
     };
-
 
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
-  cancelAction=(event)=>{
+  cancelAction = (event) => {
     this.props.history.push("/broker");
-
-  }
+  };
 
   componentDidMount() {
     if (!localStorage.getItem("token")) {
       this.props.history.push("/login");
     }
-    this.state.id=this.props.match.params.id
+    this.setState({ id: this.props.match.params.id });
     getBroker(this.state.id)
-        .then(aBroker => {
-          this.setState({name: aBroker.name,taxes:aBroker.taxes});
-
-
-        })
-        .catch(() => this.setState({ error: this.props.t("genericError") }));
+      .then((aBroker) => {
+        this.setState({ name: aBroker.name, taxes: aBroker.taxes });
+      })
+      .catch(() => this.setState({ error: this.props.t("genericError") }));
   }
 
   handleChangeName = (event) => {
@@ -52,7 +46,6 @@ class Taxes extends React.Component {
       errorToDisplay = this.props.t("comError");
     }
     this.setState({ error: errorToDisplay });
-
   }
 
   handleSubmit = (event) => {
@@ -60,68 +53,65 @@ class Taxes extends React.Component {
     postTaxCreate({
       name: this.state.taxName,
     })
-        .then((response) => {
-          this.props.history.push("/broker");
-        })
-        .catch((responseError) => this.handleAPIError(responseError));
+      .then((response) => {
+        this.props.history.push("/broker");
+      })
+      .catch((responseError) => this.handleAPIError(responseError));
   };
 
   generate() {
-    return this.state.taxes.map((each) =>
-        <ListGroup.Item action >
-          {each.name}
-        </ListGroup.Item>
-    );
+    return this.state.taxes.map((each) => (
+      <ListGroup.Item action>{each.name}</ListGroup.Item>
+    ));
   }
 
   render() {
     const { t } = this.props;
     const { id } = this.props.match.params;
 
-      return (
-
+    return (
       <div>
         <NavBarPage />
         <div className="container-fluid">
-
           <Card>
             <Card.Header as="h5">{t("brokerEdit")}</Card.Header>
             <Card.Body>
-
-
               <Form onSubmit={this.handleSubmit}>
                 <Row>
-                <Col lg="7" xs="7" md="7">
-                  <Row>
-                    <Row className="mb-3">
-                      <Form.Group className="mb-3" controlId="idValue">
-                        <Form.Label>{t("id")}</Form.Label>
-                        <Form.Control value={id}/>
-                      </Form.Group>
+                  <Col lg="7" xs="7" md="7">
+                    <Row>
+                      <Row className="mb-3">
+                        <Form.Group className="mb-3" controlId="idValue">
+                          <Form.Label>{t("id")}</Form.Label>
+                          <Form.Control value={id} />
+                        </Form.Group>
+                      </Row>
+
+                      <Row className="mb-3">
+                        <Form.Group className="mb-3" controlId="nameValue">
+                          <Form.Label>{t("name")}</Form.Label>
+                          <Form.Control
+                            onChange={this.handleChangeName}
+                            value={this.state.name}
+                          />
+                        </Form.Group>
+                      </Row>
                     </Row>
+                  </Col>
 
-                    <Row className="mb-3">
-                      <Form.Group className="mb-3" controlId="nameValue">
-                        <Form.Label>{t("name")}</Form.Label>
-                        <Form.Control onChange={this.handleChangeName} value={this.state.name}/>
-                      </Form.Group>
+                  <Col lg="4" xs="4" md="4">
+                    <Row>
+                      <NavLink to={"/broker/edit/" + this.state.id + "/tax"}>
+                        {t("brokerAddTax")}
+                      </NavLink>
                     </Row>
-
-
-                  </Row>
-                </Col>
-
-                <Col lg="4" xs="4" md="4">
-                  <Row>
-                    <NavLink to={"/broker/edit/"+this.state.id+"/tax"}>
-                      {t("brokerAddTax")}
-                    </NavLink>
-                  </Row>
-                  <Row>
-
-                  <ListGroup defaultActiveKey="#link1"> {this.generate()} </ListGroup>
-                  </Row>
-                </Col>
+                    <Row>
+                      <ListGroup defaultActiveKey="#link1">
+                        {" "}
+                        {this.generate()}{" "}
+                      </ListGroup>
+                    </Row>
+                  </Col>
                 </Row>
 
                 <Row>
@@ -129,15 +119,17 @@ class Taxes extends React.Component {
                     {t("save")}
                   </Button>
 
-                  <Button variant="outline-primary" type="cancel" onClick={this.cancelAction}>
+                  <Button
+                    variant="outline-primary"
+                    type="cancel"
+                    onClick={this.cancelAction}
+                  >
                     {t("cancel")}
                   </Button>
                 </Row>
-
               </Form>
             </Card.Body>
           </Card>
-
         </div>
       </div>
     );
