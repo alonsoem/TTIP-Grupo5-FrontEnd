bd358536393a154857330fc2b2b3ad06f30475d9
Asta86 <psljp@protonmail.com>
2021-10-25 01:12:59 -0300

    Add file RegisterForm.test.js

---
+++ src/test/RegisterForm.test.js
@@ -0,0 +1,43 @@
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import RegisterForm from "../components/RegisterForm";
+
+global.localStorage = {
+  getItem: () => undefined,
+};
+
+const history = createMemoryHistory();
+history.push("/");
+
+const setup = () => {
+  return render(
+    <MemoryRouter>
+      <RegisterForm history={history} />
+    </MemoryRouter>
+  );
+};
+
+test("Layout works", () => {
+  const { container } = setup();
+
+  expect(container.querySelector('[type="submit"]')).toHaveTextContent(
+    "Submit"
+  );
+});
+
+test("No username or password shows error messages", async () => {
+  const { container } = setup();
+
+  fireEvent.click(container.querySelector('[type="submit"]'));
+
+  expect(await screen.findByText("Email is invalid or missing")).toBeVisible();
+  expect(
+    await screen.findByText("Password must be at least 4 characters")
+  ).toBeVisible();
+});
