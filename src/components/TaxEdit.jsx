b119b2ccf94f164751ba912847e3efa88944dc35
Asta86 <psljp@protonmail.com>
2021-10-30 19:13:31 -0300

    Use button and tooltip when adding taxes and rules

--- src/components/TaxEdit.jsx
+++ src/components/TaxEdit.jsx
@@ -1,46 +1,48 @@
 import React from "react";
-import { withTranslation } from "react-i18next";
-import {getTax, putTaxEdit} from "../api/api";
-import NavBarPage from "./NavBarPage";
-import {Card, Row, Form, Button, Col} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import ListGroup from "react-bootstrap/ListGroup";
+import { withTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import { getTax, putTaxEdit } from "../api/api";
 import DragTest from "./dragTest";
-
+import NavBarPage from "./NavBarPage";
 
 class Taxes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id:0,
+      id: 0,
       name: "",
-      url:"",
-      rules:[],
-      errors:[],
+      url: "",
+      rules: [],
+      errors: [],
     };
-
 
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleChangeUrl = this.handleChangeUrl.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   getAllRules() {
-    return <DragTest taxRules={this.state.rules.map(e=>({id:e.id,name:e.name}))} context={this}/>;
+    return (
+      <DragTest
+        taxRules={this.state.rules.map((e) => ({ id: e.id, name: e.name }))}
+        context={this}
+      />
+    );
   }
 
-  cancelAction=(event)=>{
+  cancelAction = (event) => {
     event.preventDefault();
     this.props.history.goBack();
     //this.props.history.push("/broker");
-  }
+  };
 
   componentDidMount() {
     if (!localStorage.getItem("token")) {
       this.props.history.push("/login");
     }
-    this.state.id=this.props.match.params.id
+    this.state.id = this.props.match.params.id;
     this.update();
   }
 
@@ -58,182 +60,173 @@ class Taxes extends React.Component {
       errorToDisplay = this.props.t("comError");
     }
     this.setState({ error: errorToDisplay });
-
   }
 
-    submit = () => {
-        putTaxEdit(this.state.id,{
-            name: this.state.name,
-            url: this.state.url
-        })
-            .then((response) => {
-                this.props.history.push("/broker");
-            })
-            .catch((responseError) => this.handleAPIError(responseError));
-    };
+  submit = () => {
+    putTaxEdit(this.state.id, {
+      name: this.state.name,
+      url: this.state.url,
+    })
+      .then((response) => {
+        this.props.history.push("/broker");
+      })
+      .catch((responseError) => this.handleAPIError(responseError));
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        var errors = [];
+  handleSubmit = (event) => {
+    event.preventDefault();
+    var errors = [];
 
-        // Check name of user
-        if (this.state.name === "") {
-            errors.push("name");
-        }
-
-        // Check email address
-        const expression = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
-
-        var validExpression = expression.test(String(this.state.url).toLowerCase());
-        if (!validExpression) {
-            errors.push("url");
-        }
-
-        this.setState({
-            errors: errors,
-        });
-
-        if (errors.length > 0) {
-            return false;
-        } else {
-            this.submit();
-        }
-    };
-
-    hasError(key) {
-        return this.state.errors.indexOf(key) !== -1;
+    // Check name of user
+    if (this.state.name === "") {
+      errors.push("name");
     }
 
-    update(){
-    getTax(this.state.id)
-        .then(aTax => {
-          this.setState({name: aTax.name,
-            url: aTax.url,
-            rules:aTax.rules});
-        })
+    // Check email address
+    const expression =
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
 
-        .catch(() => this.setState({ error: this.props.t("genericError") }));
+    var validExpression = expression.test(String(this.state.url).toLowerCase());
+    if (!validExpression) {
+      errors.push("url");
+    }
+
+    this.setState({
+      errors: errors,
+    });
+
+    if (errors.length > 0) {
+      return false;
+    } else {
+      this.submit();
+    }
+  };
+
+  hasError(key) {
+    return this.state.errors.indexOf(key) !== -1;
   }
 
+  update() {
+    getTax(this.state.id)
+      .then((aTax) => {
+        this.setState({ name: aTax.name, url: aTax.url, rules: aTax.rules });
+      })
 
+      .catch(() => this.setState({ error: this.props.t("genericError") }));
+  }
 
   render() {
     const { t } = this.props;
-    const idTax= this.props.match.params.id;
+    const idTax = this.props.match.params.id;
 
-
-      return (
-
+    return (
       <div>
         <NavBarPage />
         <div className="container-fluid">
-
           <Card>
             <Card.Header as="h5">{t("taxEdit")}</Card.Header>
             <Card.Body>
-
-
               <Form onSubmit={this.handleSubmit}>
                 <Row>
-                <Col lg="7" xs="7" md="7">
-                  <Row>
-                    <Row className="mb-3">
-                      <Form.Group className="mb-3" controlId="idValue">
-                        <Form.Label>{t("id")}</Form.Label>
-                        <Form.Control value={idTax}/>
-                      </Form.Group>
-                    </Row>
+                  <Col lg="7" xs="7" md="7">
+                    <Row>
+                      <Row className="mb-3">
+                        <Form.Group className="mb-3" controlId="idValue">
+                          <Form.Label>{t("id")}</Form.Label>
+                          <Form.Control value={idTax} />
+                        </Form.Group>
+                      </Row>
 
-                    <Row className="mb-3">
-                      <Form.Group className="mb-3" controlId="nameValue">
-                        <Form.Label>{t("name")}</Form.Label>
-                        <Form.Control onChange={this.handleChangeName} value={this.state.name}
-                                      className={
-                                          this.hasError("name")
-                                              ? "form-control is-invalid"
-                                              : "form-control"
-                                      }
-                        />
+                      <Row className="mb-3">
+                        <Form.Group className="mb-3" controlId="nameValue">
+                          <Form.Label>{t("name")}</Form.Label>
+                          <Form.Control
+                            onChange={this.handleChangeName}
+                            value={this.state.name}
+                            className={
+                              this.hasError("name")
+                                ? "form-control is-invalid"
+                                : "form-control"
+                            }
+                          />
                           <div
-                              className={
-                                  this.hasError("name")
-                                      ? "invalid-feedback"
-                                      : "visually-hidden"
-                              }
+                            className={
+                              this.hasError("name")
+                                ? "invalid-feedback"
+                                : "visually-hidden"
+                            }
                           >
-                              {t("userInvalidFeedback")}
+                            {t("userInvalidFeedback")}
                           </div>
-                      </Form.Group>
-
-                    </Row>
-                    <Row className="mb-3">
-                      <Form.Group className="mb-3" controlId="urlValue">
-                        <Form.Label>{t("url")}</Form.Label>
-                        <Form.Control onChange={this.handleChangeUrl} value={this.state.url}
-                                      className={
-                                          this.hasError("url")
-                                              ? "form-control is-invalid"
-                                              : "form-control"
-                                      }/>
+                        </Form.Group>
+                      </Row>
+                      <Row className="mb-3">
+                        <Form.Group className="mb-3" controlId="urlValue">
+                          <Form.Label>{t("url")}</Form.Label>
+                          <Form.Control
+                            onChange={this.handleChangeUrl}
+                            value={this.state.url}
+                            className={
+                              this.hasError("url")
+                                ? "form-control is-invalid"
+                                : "form-control"
+                            }
+                          />
                           <div
-                              className={
-                                  this.hasError("url")
-                                      ? "invalid-feedback"
-                                      : "visually-hidden"
-                              }
+                            className={
+                              this.hasError("url")
+                                ? "invalid-feedback"
+                                : "visually-hidden"
+                            }
                           >
-                              {t("invalidUrl")}
+                            {t("invalidUrl")}
                           </div>
-                      </Form.Group>
-
-
+                        </Form.Group>
+                      </Row>
                     </Row>
+                  </Col>
 
-
-                  </Row>
-                </Col>
-
-                <Col lg="4" xs="4" md="4">
+                  <Col lg="4" xs="4" md="4">
                     <Card>
-                        <Card.Header as="h5">
-                            <div className="row">
-                                <div className="col-sm-8">{t("taxAddRule")}</div>
-                                <div className="col-sm-4">
-                                    <NavLink to={"/rule/"+this.state.id}>
-                                        <Button  class={"btn-sm"}>
-                                            <i className="fa fa-plus"></i>
-                                        </Button>
-                                    </NavLink>
-                                </div>
-                            </div>
-
-
-                        </Card.Header>
-                        <Card.Body>
-                            <ListGroup defaultActiveKey="#link1"> {this.getAllRules()}</ListGroup>
-                        </Card.Body>
+                      <Card.Header as="h5">
+                        <div className="row">
+                          <div className="col-sm-4">
+                            <NavLink to={"/rule/" + this.state.id}>
+                              <Button title={t("taxAddRule")} class={"btn-sm"}>
+                                <i className="fa fa-plus"></i>
+                              </Button>
+                            </NavLink>
+                          </div>
+                        </div>
+                      </Card.Header>
+                      <Card.Body>
+                        <ListGroup defaultActiveKey="#link1">
+                          {" "}
+                          {this.getAllRules()}
+                        </ListGroup>
+                      </Card.Body>
                     </Card>
-
-                </Col>
+                  </Col>
                 </Row>
 
-                  <Row class={"justify-content-start"}>
-                      <Col className="justify-content-start text-left">
+                <Row class={"justify-content-start"}>
+                  <Col className="justify-content-start text-left">
+                    <Button variant="primary" type="submit">
+                      {t("update")}
+                    </Button>
 
-                  <Button variant="primary" type="submit">
-                    {t("update")}
-                  </Button>
-
-                  <Button variant="outline-primary" type="cancel" onClick={this.cancelAction}>
-                    {t("back")}
-                  </Button>
-                      </Col>
+                    <Button
+                      variant="outline-primary"
+                      type="cancel"
+                      onClick={this.cancelAction}
+                    >
+                      {t("back")}
+                    </Button>
+                  </Col>
                 </Row>
-
               </Form>
             </Card.Body>
           </Card>
-
         </div>
       </div>
     );
