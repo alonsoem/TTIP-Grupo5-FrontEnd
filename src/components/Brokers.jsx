fdb6a38bc2ca5c191fdd6449f8efbf0af1292316
Asta86 <psljp@protonmail.com>
2021-10-31 17:19:53 -0300

    Update position of the button used to add a broker

--- src/components/Brokers.jsx
+++ src/components/Brokers.jsx
@@ -2,9 +2,9 @@ import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
 import React from "react";
 import { withTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
-import {deleteBroker, getBrokers} from "../api/api";
+import { deleteBroker, getBrokers } from "../api/api";
 import NavBarPage from "./NavBarPage";
-import {Button} from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 class Brokers extends React.Component {
   constructor(props) {
@@ -21,31 +21,31 @@ class Brokers extends React.Component {
     this.updateBrokers();
   }
 
-  updateBrokers(){
+  updateBrokers() {
     getBrokers()
-        .then((taxes) => {
-          console.log(taxes);
-          this.setState({ rows: taxes });
-        })
-        .catch(() => this.setState({ error: this.props.t("genericError") }));
+      .then((taxes) => {
+        console.log(taxes);
+        this.setState({ rows: taxes });
+      })
+      .catch(() => this.setState({ error: this.props.t("genericError") }));
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
     deleteBroker(event.target.id)
-        .then((response) => {
-            this.updateBrokers();
-        })
-        .catch((responseError) => this.handleAPIError(responseError));
+      .then((response) => {
+        this.updateBrokers();
+      })
+      .catch((responseError) => this.handleAPIError(responseError));
   };
 
-  editBroker=(event)=>{
+  editBroker = (event) => {
     this.props.history.push("/broker/edit/" + event.target.id);
-  }
+  };
 
-  useBroker=(event)=>{
+  useBroker = (event) => {
     this.props.history.push("/maincalc/" + event.target.id);
-  }
+  };
 
   render() {
     const { t } = this.props;
@@ -53,16 +53,21 @@ class Brokers extends React.Component {
       return {
         id: item.id,
         name: item.name,
-        url: <Button variant="secondary" onClick={this.editBroker} id={item.id}>
-                <i className="fa fa-edit"></i>
-             </Button>,
-          del: <Button variant="secondary" onClick={this.handleSubmit} id={item.id}>
-                  <i className="fa fa-minus"></i>
-               </Button>,
-          calc: <Button variant="secondary" onClick={this.useBroker} id={item.id}>
+        url: (
+          <Button variant="secondary" onClick={this.editBroker} id={item.id}>
+            <i className="fa fa-edit"></i>
+          </Button>
+        ),
+        del: (
+          <Button variant="secondary" onClick={this.handleSubmit} id={item.id}>
+            <i className="fa fa-minus"></i>
+          </Button>
+        ),
+        calc: (
+          <Button variant="secondary" onClick={this.useBroker} id={item.id}>
             <i className="fa fa-calculator"></i>
-          </Button>,
-
+          </Button>
+        ),
       };
     });
     const columns = [
@@ -94,19 +99,27 @@ class Brokers extends React.Component {
         <div className="container-fluid">
           <h1 className="card-header">{t("brokers")}</h1>
           <br />
-          <NavLink to="/broker/edit">
-            <i class="fas fa-plus-square fa-5x" title={t("brokerNew")}></i>
-          </NavLink>
-          <br />
-
-          <br />
           <div className="form-content" align="center">
             <div className="row">
               <div id={"contenedor"}>
-                <MDBTable className="table-hover table table-success table-striped" responsive>
-                  <MDBTableHead columns={columns} />
-                  <MDBTableBody rows={dataSet} />
-                </MDBTable>
+                <Row className="row">
+                  <Col className="col-12 col-sm-4 col-lg-6 col-xl-6 pb-4">
+                    <MDBTable
+                      className="table-hover table table-success table-striped"
+                      responsive
+                    >
+                      <MDBTableHead columns={columns} />
+                      <MDBTableBody rows={dataSet} />
+                    </MDBTable>
+                  </Col>
+                  <Col className="col-1 col-sm-1 col-lg-1 col-xl-1 pb-4">
+                    <NavLink to="/broker/edit">
+                      <Button title={t("brokerNew")} class={"btn-sm"}>
+                        <i className="fa fa-plus"></i>
+                      </Button>
+                    </NavLink>
+                  </Col>
+                </Row>
               </div>
             </div>
           </div>
