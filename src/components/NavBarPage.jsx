a70c2f7ea2cb41d4a99cf54371f1187002c0f56a
alonsoem <alonso.em@gmail.com>
2021-11-13 02:06:38 -0300

    Logo renovado

--- src/components/NavBarPage.jsx
+++ src/components/NavBarPage.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "react-bootstrap";
 import { withTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import App18 from "../App18.jsx";
-import aleclogo from "../static/alecLogoTrans.png";
+import aleclogo from "../static/alecLogoTrans2.png";
 
 class NavBarPage extends Component {
   render() {
